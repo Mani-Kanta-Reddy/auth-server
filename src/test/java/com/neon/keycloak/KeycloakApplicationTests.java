cae1f21f289b23e94033598a76055224e87e754b
Ummadi, Mani Kanta Reddy <ManiKantaReddy_Ummadi@comcast.com>
2023-06-10 16:04:46 +0530

    initial commit project bootstrap

---
+++ src/test/java/com/neon/keycloak/KeycloakApplicationTests.java
@@ -0,0 +1,14 @@
+package com.neon.keycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+}
